749cd64a70777702fa58cb08c242df1c21fd6dd8
59 <5.9.devv@gmail.com>
2022-03-01 23:47:39 +0900

    init and make Collection View

---
+++ MusicPlayer/Home/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 59 on 2022/03/01.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+}
---
+++ MusicPlayer/Home/TrackCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TrackCollectionViewCell.swift
+//  MusicPlayer
+//
+//  Created by 59 on 2022/03/01.
+//
+
+import UIKit
+
+class TrackCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var trackThumbnail: UIImageView!
+    @IBOutlet weak var trackTitle: UILabel!
+    @IBOutlet weak var trackArtist: UILabel!
+    
+    
+}
