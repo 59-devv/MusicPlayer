29c72276fbfe5e024e4acf655eb89020ac7b82b6
59 <5.9.devv@gmail.com>
2022-03-02 01:55:55 +0900

    음원 추가, 셀 크기 설정

--- MusicPlayer/Home/HomeViewController.swift
+++ MusicPlayer/Home/HomeViewController.swift
@@ -16,7 +16,16 @@ class HomeViewController: UIViewController {
 }
 
 extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrackCollectionViewCell", for: indexPath) as? TrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
 }
 
 extension HomeViewController: UICollectionViewDelegate {
@@ -24,5 +33,12 @@ extension HomeViewController: UICollectionViewDelegate {
 }
 
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 20
+        let width:CGFloat = (collectionView.bounds.width - itemSpacing) / 2
+        let letterHeight: CGFloat = 60
+        let height = (width + letterHeight)
+        
+        return CGSize(width: width, height: height)
+    }
 }
