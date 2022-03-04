85b446880594bf81641d6cf4a87549d9bff8f62c
59 <5.9.devv@gmail.com>
2022-03-04 12:28:19 +0900

    PlyaerViewController 화면 구성 완료

--- MusicPlayer/Player/PlayerViewController.swift
+++ MusicPlayer/Player/PlayerViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class PlayerViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var playControlButton: UIButton!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var totalDurationLabel: UILabel!
+    
+    // Simple Player를 만들고, 프로퍼티 추가하기
+    var timeObserver: Any?
+    var isSeeking: Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
