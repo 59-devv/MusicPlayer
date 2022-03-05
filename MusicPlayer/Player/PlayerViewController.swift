64737a01ace2966a47ddda9d602c4de1909a12a0
59 <5.9.devv@gmail.com>
2022-03-05 22:08:12 +0900

    플레이어뷰로 곡 정보 넘기기 및 재생 구현

--- MusicPlayer/Player/PlayerViewController.swift
+++ MusicPlayer/Player/PlayerViewController.swift
@@ -26,5 +26,57 @@ class PlayerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateTrackInfo()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    @IBAction func beingDrag(_ sender: UISlider) {
+        
+    }
+    
+    @IBAction func endDrag(_ sender: UISlider) {
+    }
+    
+    @IBAction func seek(_ sender: UISlider) {
+    }
+    
+    @IBAction func togglePlayButton(_ sender: UIButton) {
+        if simplePlayer.isPlaying {
+            simplePlayer.pause()
+        } else {
+            simplePlayer.play()
+        }
+        
+        updatePlayButton()
+    }
+}
+
+extension PlayerViewController {
+    func updateTrackInfo() {
+        // 트랙 정보 업데이트
+        guard let track = simplePlayer.currentItem?.convertToTrack() else { return }
+        thumbnailImageView.image = track.artwork
+        titleLabel.text = track.title
+        artistLabel.text = track.artist
+    }
+    
+    func updateTintColor() {
+//        playControlButton.tintColor = DefaultStyle.Colors.tint
+//        timeSlider.tintColor = DefaultStyle.Colors.tint
+    }
+    
+    func updatePlayButton() {
+        if simplePlayer.isPlaying {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "pause.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        } else {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "play.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        }
     }
 }
